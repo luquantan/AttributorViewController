e2c5b1fecfcd36ffff904d6ed870c12bfe5de1f6
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2014-10-20 09:30:46 -0400

    Initial Commit.

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by LuQuan Intrepid on 10/20/14.
+//  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by LuQuan Intrepid on 10/20/14.
+//  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
