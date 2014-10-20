0d0f31ae556e2092bbc2b48b539d133f2e629e68
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2014-10-20 10:20:07 -0400

    Second Commit. AttributorViewController is done. Warnings still remain for misplaced views.

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -9,10 +9,38 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UITextView *body;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundColorOfButton:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+//- (IBAction)outlineBodySelection:(id)sender
+//{
+//    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3 ,
+//                                           NSStrokeColorAttributeName : [UIColor blackColor]}
+//                                   range:self.body.selectedRange];
+//}
+
+- (IBAction)outlineBodySelection:(UIButton *)sender
+{
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3 ,
+                                           NSStrokeColorAttributeName : [UIColor blackColor]}
+                                           range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection:(UIButton *)sender
+{
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
